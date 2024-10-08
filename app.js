57e732d9b0e885c8e6874ac6e8f1842628417c24
devmulkalwar <devmulkalwar95@gmail.com>
2024-10-08 15:23:45 +0530

    ischecked problem solved

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let todoList = [];
-let isChecked ;
+
 // Load todoList from localStorage on page load
 window.onload = function () {
   let storedTodoList = localStorage.getItem('todoList');
@@ -16,7 +16,7 @@ function addTodo() {
     alert("Input field cannot be empty !!")
     return;
   }
-  todoList.push({ item: todoItem });
+  todoList.push({ item: todoItem, isChecked: false });
 
   // Save todoList to localStorage
   localStorage.setItem('todoList', JSON.stringify(todoList));
@@ -26,19 +26,14 @@ function addTodo() {
 }
 
 function editToDo(i) {
-  let spanEle = document.getElementById(`id_${i}`)
-  let todoItem = spanEle.value;
-  let userInput = prompt("Edit ToDo", todoItem);
+  let spanEle = document.getElementById(`id_${i}`);
+  let userInput = prompt("Edit ToDo", spanEle.innerText);
 
-  if (userInput !== "") {
-    if (userInput !== null) {
-      console.log('User input:', userInput);
-      spanEle.innerText = userInput;
-      todoList.splice(i, 1, { item: userInput });
-
-      // Update todoList in localStorage after editing
-      localStorage.setItem('todoList', JSON.stringify(todoList));
-    }
+  if (userInput !== "" && userInput !== null) {
+    console.log('User input:', userInput);
+    spanEle.innerText = userInput;
+    todoList[i].item = userInput; // Update the item
+    localStorage.setItem('todoList', JSON.stringify(todoList)); // Update localStorage
   }
 }
 
@@ -53,29 +48,29 @@ function deleteToDo(i) {
 
 function checkItem(i) {
   let checkbox = document.getElementById(`chekbox_${i}`);
-  if (checkbox.checked) {
-    let spanEle = document.getElementById(`id_${i}`);
-    spanEle.style.textDecoration = 'line-through';
-  }
-  else {
-    let spanEle = document.getElementById(`id_${i}`);
-    spanEle.style.textDecoration = 'none';
-  }
+  todoList[i].isChecked = checkbox.checked; // Update the isChecked property
+
+  // Update the display
+  let spanEle = document.getElementById(`id_${i}`);
+  spanEle.style.textDecoration = checkbox.checked ? 'line-through' : 'none';
+
+  // Update localStorage
+  localStorage.setItem('todoList', JSON.stringify(todoList));
 }
 
 function displayItems() {
   let containerElement = document.querySelector('.todo-container');
   let newHtml = '';
   for (let i = 0; i < todoList.length; i++) {
-    let { item } = todoList[i];
+    let { item, isChecked } = todoList[i];
     newHtml += `
-        <li class= "list-item" >
-        <input type="checkbox" class="check-box" id="chekbox_${i}" onchange="checkItem(${i})"/>
-        <span id = "id_${i}">${item}</span>
+      <li class="list-item">
+        <input type="checkbox" class="check-box" id="chekbox_${i}" onchange="checkItem(${i})" ${isChecked ? 'checked' : ''}/>
+        <span id="id_${i}" style="text-decoration: ${isChecked ? 'line-through' : 'none'};">${item}</span>
         <button class='btn-edit' onclick="editToDo(${i})">Edit</button>
         <button class='btn-delete' onclick="deleteToDo(${i})">Delete</button>
-        </li>
-      `;
+      </li>
+    `;
   }
   containerElement.innerHTML = newHtml;
 }
